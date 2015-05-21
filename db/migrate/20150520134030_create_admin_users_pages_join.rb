4a68bd9fb6514fb92b9977698d71507ea1247e34
shyakaster <shyakaster@gmail.com>
2015-05-21 12:46:50 +0300

    Add relationships and associations

--- db/migrate/20150520134030_create_admin_users_pages_join.rb
+++ db/migrate/20150520134030_create_admin_users_pages_join.rb
@@ -3,6 +3,8 @@ class CreateAdminUsersPagesJoin < ActiveRecord::Migration
     create_table :admin_users_pages, :id=>false do |t|
       t.integer "admin_user_id"
       t.integer "page_id"
+      # t.references :admin_user
+      # t.references :page
     end
     add_index :admin_users_pages, ["admin_user_id","page_id"]
   end
